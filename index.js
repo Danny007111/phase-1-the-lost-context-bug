36bbc9c9a1189bdd936ff5476f5f27e24237a524
Danny Rodriguez <danny007111@hotmail.com>
2022-05-20 13:00:52 -0500

    Completed code-along

--- index.js
+++ index.js
@@ -10,3 +10,50 @@ const messageConfig = {
       "Loki"
   ]
 };
+
+
+const printCard = function () {
+  console.log(this.frontContent);
+  console.log(this.insideContent);
+
+  // this.signatories.forEach(function (signatory) {
+  //   const message = `${this.closing[signatory]}, ${signatory}`;
+  //   console.log(message);
+  // }, this);
+
+/////////////////////////////////////////////////////////////////////////////////////
+  // const contextBoundForEachExpr = function (signatory) {
+  //   const message = `${this.closing[signatory]}, ${signatory}`;
+  //   console.log(message);
+  // }.bind(this);
+
+  // this.signatories.forEach(contextBoundForEachExpr);
+
+/////////////////////////////////////////////////////////////////////////////////////
+  // const outerContext = this;
+
+  // this.signatories.forEach(function (signatory) {
+  //   const message = `${outerContext.closing[signatory]}, ${signatory}`;
+  //   console.log(message);
+  // });
+
+/////////////////////////////////////////////////////////////////////////////////////
+  this.signatories.forEach((signatory) =>
+    console.log(`${this.closing[signatory]}, ${signatory}`)
+  );
+};
+
+printCard.call(messageConfig);
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+// The `const greeter` is merely the assignment, the expression begins after the `=`
+const greeter = (nameToGreet) => {
+  const message = `Good morning ${nameToGreet}`;
+  console.log(message);
+  return "Greeted: " + nameToGreet;
+};
+const result = greeter("Max"); //=> "Greeted: Max"
+
+console.log(result);
